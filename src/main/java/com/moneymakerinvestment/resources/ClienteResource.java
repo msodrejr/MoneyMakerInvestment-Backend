9b5225b9ca3d5c174ce95ab61f3c35ec1911f288
Mauricio Sodre <mss.junior@globo.com>
2019-04-10 02:28:55 -0300

    feat: add ClienteService and exceptions

--- src/main/java/com/moneymakerinvestment/resources/ClienteResource.java
+++ src/main/java/com/moneymakerinvestment/resources/ClienteResource.java
@@ -1,27 +1,27 @@
 package com.moneymakerinvestment.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moneymakerinvestment.domain.Cliente;
-import com.moneymakerinvestment.domain.enums.TipoCliente;
+import com.moneymakerinvestment.domain.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Cliente> listar() {
-		Cliente cli = new Cliente(1, "Mauricio Junior", "mss.junior@globo.com", "123456789", TipoCliente.PESSOAFISICA);
-		Cliente cli2 = new Cliente(2, "Rolando Caio da Rocha", "RolandoCaio@globo.com", "123459876", TipoCliente.PESSOAFISICA);
-		List<Cliente> lista = new ArrayList<>();
-		lista.add(cli);
-		lista.add(cli2);
-		return lista;
+	@Autowired
+	private ClienteService clienteService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Cliente> findOne(@PathVariable Integer id) {
+		Cliente obj = clienteService.findOne(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
