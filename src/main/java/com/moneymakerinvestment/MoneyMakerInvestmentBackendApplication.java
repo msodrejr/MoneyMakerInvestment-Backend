02f7195739552e7e0d1dd5ae98aa623bca5792e3
Mauricio Sodre <mss.junior@globo.com>
2019-04-10 00:44:17 -0300

    Projeto criado

---
+++ src/main/java/com/moneymakerinvestment/MoneyMakerInvestmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moneymakerinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMakerInvestmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMakerInvestmentBackendApplication.class, args);
+	}
+
+}
